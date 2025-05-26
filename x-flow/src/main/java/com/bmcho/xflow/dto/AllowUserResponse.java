b6e7bd8928b7dfe5a6769f69cac9e1f5dd9a9e65
bmcho12 <bmcho12@nm-metaworld.com>
2025-05-26 16:47:08 +0900

    enh: 진입요청 api

---
+++ x-flow/src/main/java/com/bmcho/xflow/dto/AllowUserResponse.java
@@ -0,0 +1,4 @@
+package com.bmcho.xflow.dto;
+
+public record AllowUserResponse(Long requestCount, Long allowedCount) {
+}
---
+++ x-flow/src/main/java/com/bmcho/xflow/dto/AllowedUserResponse.java
@@ -0,0 +1,4 @@
+package com.bmcho.xflow.dto;
+
+public record AllowedUserResponse(Boolean allowed) {
+}
