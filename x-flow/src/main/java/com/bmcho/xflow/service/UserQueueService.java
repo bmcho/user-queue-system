5c3308b4cee9994630a2d9375c3d38e974b0c782
bmcho12 <bmcho12@nm-metaworld.com>
2025-05-26 17:47:40 +0900

    enh: 대기열 스케쥴러

--- x-flow/src/main/java/com/bmcho/xflow/service/UserQueueService.java
+++ x-flow/src/main/java/com/bmcho/xflow/service/UserQueueService.java
@@ -2,13 +2,19 @@ package com.bmcho.xflow.service;
 
 import com.bmcho.xflow.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 import java.util.function.Function;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -16,6 +22,10 @@ public class UserQueueService {
 
     Function<String, String> userQueueWaitKey = "users:queue:%s:wait"::formatted;
     Function<String, String> userQueueProceedKey = "users:queue:%s:proceed"::formatted;
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
+
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimestamp = Instant.now().getEpochSecond();
@@ -43,6 +53,26 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(userQueueWaitKey.apply(queue), userId.toString())
             .defaultIfEmpty(-1L)
             .map(rank -> rank >= 0 ? rank + 1: rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+        if (!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
+        log.info("called scheduling...");
+
+        var maxAllowUserCount = 3L;
+        String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                .count(100)
+                .build())
+            .map(key -> key.split(":")[2])
+            .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+            .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+            .subscribe();
     }
 
 }
